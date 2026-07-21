85ea8f094ab31256ec9dd5117864e26a8af5b8f9
maju <108555207+mjsoweb@users.noreply.github.com>
2026-07-21 13:59:51 -0300

    Update Professor.js

--- projeto/pessoas/Professor.js
+++ projeto/pessoas/Professor.js
@@ -1,43 +1,38 @@
 // Arquivo: projeto/pessoas/Professor.js
 
-// Importamos a classe mãe Pessoa (na mesma pasta)
+// pessoas/Professor.js
 const Pessoa = require('./Pessoa');
 
-// A classe Professor herda tudo de Pessoa
 class Professor extends Pessoa {
-  #disciplina; // Atributo privado específico do Professor
+    #disciplina;
 
-  constructor() {
-    super(); // Inicializa a classe mãe Pessoa
-  }
-
-  // 1. Métodos da Disciplina (Padrão)
-  setDisciplina(disciplina) {
-    if (disciplina) {
-      this.#disciplina = disciplina;
-      return true;
+    constructor() {
+        super();
+        this.#disciplina = "";
     }
-    return false;
-  }
 
-  getDisciplina() {
-    return this.#disciplina;
-  }
-
-  // 2. Sobrescrevendo o método setEmail()
-  setEmail(email) {
-    // Regra do professor: precisa terminar com ".edu.br"
-    // Usamos o método .endsWith() do JavaScript para checar o final do texto
-    if (email.endsWith('.edu.br')) {
-      // Se passar na nossa regra, chamamos o setEmail da classe mãe (Pessoa)
-      // para que ela faça a validação da biblioteca e grave o dado se estiver correto
-      return super.setEmail(email);
-    } else {
-      // Se não terminar com .edu.br, já recusa direto
-      return false;
+    getDisciplina() {
+        return this.#disciplina;
     }
-  }
+
+    setDisciplina(disciplina) {
+        if (disciplina && disciplina.trim() !== "") {
+            this.#disciplina = disciplina;
+            return true;
+        }
+        return false;
+    }
+
+    // Sobrescrita de método (Override) exigindo .edu.br para professores
+    setEmail(email) {
+        if (email && String(email).trim().endsWith(".edu.br")) {
+            // Reutilizando o método da classe pai (Pessoa) que valida o restante
+            return super.setEmail(email);
+        }
+        return false;
+    }
 }
 
+module.exports = Professor;
 // Exportamos a classe Professor
 module.exports = Professor;
